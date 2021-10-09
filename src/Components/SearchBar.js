1cae591f7789b17330942cd8601b3779ac93a5cc
jreyes8631 <jreyes8631@gmail.com>
2021-10-09 11:57:27 -0400

    add style to search bar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 class SearchBar extends React.Component {
   render() {
-      return <div> 
-          <form>
+      return <div className="ui segment"> 
+          <form className="ui form">
+              <div className="field">
+              <label>Image Search:</label>
               <input 
               type="text"
-              placeholder="Search"
-              label="Search"/>
+              placeholder="Example: Audi"/>
+              </div> 
           </form>
       </div>
   }
