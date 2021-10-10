d266c8de06eff2401bf201880f2e0f99bb0978b0
jreyes8631 <jreyes8631@gmail.com>
2021-10-10 19:07:45 -0400

    add a orevent default to the search bar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,19 +2,22 @@ import React from 'react'
 
 class SearchBar extends React.Component {
  
- onInputeChange(event){
-  console.log(event.target.value)
+ state = { term: " "};
+
+ onFormSubmit(event){
+     event.preventDefault();
  }
 
   render() {
       return <div className="ui segment"> 
-          <form className="ui form">
+          <form onSubmit={this.onFormSubmit}className="ui form">
               <div className="field">
               <label>Image Search:</label>
               <input 
               type="text"
+              value={this.state.term}
               placeholder="Example: Audi"
-              onChange={this.onInputeChange}/>
+              onChange={e => this.setState({ term: e.target.value})}/>
               </div> 
           </form>
       </div>
