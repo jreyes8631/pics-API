8e8c48913c5d0423983063a47c9f5d39446602d4
jreyes8631 <jreyes8631@gmail.com>
2021-10-31 23:25:34 -0400

    getting the height of the images

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -9,9 +9,13 @@ class ImageCard extends React.Component {
     }
 
     componentDidMount(){
-        console.log(this.imageRef)
+        this.imageRef.current.addEventListener('load', this.setSpans)
     }
     
+    setSpans = () => {
+       console.log(this.imageRef.current.clientHeight)
+    }
+
     render() {
         const {description, urls} = this.props.image
         return(
